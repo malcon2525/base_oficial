e66741cbe78677110440869d84f32f051a767e74
Malcon Toledo <malcon.toledo@paranacidade.org.br>
2024-12-18 10:04:54 -0300

    menu-lateral e menu topo - construindo

--- resources/views/layouts/app/adm/adm.blade.php
+++ resources/views/layouts/app/adm/adm.blade.php
@@ -16,6 +16,34 @@
 </head>
 <body>
     
+    {{-- menu topo --}}
+    <div class="container">
+        
+        <div class="d-flex mb-3">
+            <div class="logo align-self-center me-3">
+                Manager On-click
+            </div>
+            <div class=" lign-self-center me-3">
+                <a class="" data-bs-toggle="offcanvas" href="#menuLateralADM" role="button" aria-controls="offcanvasExample">
+                    <svg class="sanduiche" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </a>
+            </div>
+            <div class="menu-topo flex-fill text-end align-self-center">
+                menu
+            </div>
+        </div>
+    </div>
+
+
+    {{-- menu lateral esquerdo --}}
+    @include('layouts.app.adm.menuadm')
+    
+    
+    
+    
+    {{-- CONTEÚDO --}}
     @yield('content')
 
     
